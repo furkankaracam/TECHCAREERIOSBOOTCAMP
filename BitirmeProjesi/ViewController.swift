e0f4831ba45b7ac02f5c33e2865212476f7290f7
furkankaracam <furkankaracam2706@gmail.com>
2022-03-22 16:38:12 +0300

    Initial Commit

---
+++ BitirmeProjesi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BitirmeProjesi
+//
+//  Created by FURKAN KARAÇAM on 22.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
